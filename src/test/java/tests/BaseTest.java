d9a72c3b3ddc98719d0b08cdfba9008b49062a7b
Thanh Nguyen <vnscriptkid@gmail.com>
2020-08-28 14:56:14 +0700

    Run tests on cloud using SauceLabs

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,30 +2,65 @@ package tests;
 
 import org.junit.Rule;
 import org.junit.rules.ExternalResource;
+import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.lang.reflect.Method;
+import java.net.URL;
+
 import static tests.Config.*;
 
 public class BaseTest {
     protected WebDriver driver;
+    private String testName;
 
     @Rule
     public ExternalResource resource = new ExternalResource() {
         @Override
         protected void before() throws Throwable {
-            if (browserName.equals("firefox")) {
-                System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/vendor/geckodriver.exe");
-                driver = new FirefoxDriver();
-            } else if (browserName.equals("chrome")) {
-                System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/vendor/chromedriver.exe");
-                driver = new ChromeDriver();
+            if (host.equals("saucelabs")) {
+                if (sauceKey == null || sauceUser == null) {
+                    throw new Exception("Sauce credentials is not set up yet");
+                }
+                DesiredCapabilities capabilities = new DesiredCapabilities();
+                capabilities.setCapability("username", sauceUser);
+                capabilities.setCapability("accessKey", sauceKey);
+
+                capabilities.setCapability("browserName", browserName);
+                capabilities.setCapability("platform", platform);
+                capabilities.setCapability("version", version);
+                capabilities.setCapability("name", testName);
+
+                driver = new RemoteWebDriver(new URL("https://ondemand.us-west-1.saucelabs.com:443/wd/hub"), capabilities);
+            } else if (host.equals("localhost")) {
+                if (browserName.equals("firefox")) {
+                    System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/vendor/geckodriver.exe");
+                    driver = new FirefoxDriver();
+                } else if (browserName.equals("chrome")) {
+                    System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/vendor/chromedriver.exe");
+                    driver = new ChromeDriver();
+                }
             }
+
         }
 
         @Override
         protected void after() {
             driver.quit();
+        }
+    };
+
+    @Rule
+    public TestRule watcher = new TestWatcher() {
+        @Override
+        protected void starting(Description description) {
+            testName = description.getDisplayName();
         }
     };
 }
--- src/test/java/tests/Config.java
+++ src/test/java/tests/Config.java
@@ -2,5 +2,10 @@ package tests;
 
 public class Config {
     public static final String baseUrl = System.getProperty("baseUrl", "https://the-internet.herokuapp.com/");
-    public static final String browserName = System.getProperty("browserName", "chrome");
+    public static final String host = System.getProperty("host", "saucelabs");
+    public static final String browserName = System.getProperty("browserName", "Safari");
+    public static final String version = System.getProperty("browserVersion", "11.1");
+    public static final String platform = System.getProperty("platformName", "macOS 10.13");
+    public static final String sauceUser = System.getenv("SAUCE_USERNAME");
+    public static final String sauceKey = System.getenv("SAUCE_ACCESS_KEY");
 }
